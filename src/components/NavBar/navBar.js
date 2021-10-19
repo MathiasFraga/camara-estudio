187502f1f96515008179917683c0119f89e03516
mfs1990 <mathiasfraga@hotmail.com>
2021-10-19 02:14:40 -0300

    Entrega clase 5

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './styleNav.css';
-import CartWidget from "../CartWidget/cartWidget.js";
+import CartWidget from "../CartWidget/cartWidget";
 
 const NavBar = () => {
   return (
