78a90a68161c5733210428926ee6943b46b22c91
mfs1990 <mathiasfraga@hotmail.com>
2021-11-16 23:11:27 -0300

    Entrega desafio context

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import ItemDetailContainer from './container/ItemDetailContainer/ItemDetailConta
 import ListContainer from './container/ItemListContainer/ItemListContainer';
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import  CartState  from './context/cart/CartState'
+import  Cart  from './components/CartDetails/cart'
 
 
 function App() {
   return (
+    <CartState>
       <BrowserRouter>
         <NavBar/>
         <Routes>
           <Route exact path="/" element ={<ListContainer greeting="Camara Estudio" />} />
           <Route exact path="/categoria/:categoryID" element ={<ListContainer greeting="Cursos y servicios de Fotografia" />}/>
           <Route exact path="/item/:itemID" element = {<ItemDetailContainer />} />
+          <Route exact path="/cart" element = {<Cart />} />
+          <Route path="/*" element = {<h1>Página no encontrada</h1>} />
         </Routes>
       </BrowserRouter>
+      </CartState>
     );
   }
 export default App;
+
--- src/components/ItemDetail/itemDetail.js
+++ src/components/ItemDetail/itemDetail.js
@@ -1,52 +1,98 @@
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
 import './itemDetail.css';
 import StockContador from '../ItemCount/itemCount';
+import CartContext from "../../context/cart/CartContext";
+import { NavLink } from "react-router-dom";
 
 
 const ItemDetails = ({curso}) => {
 
-    const [cupos, setCupos]= useState(1);
-    const [flag, setFlag] = useState(true);
-
-    function add() {
-      if (cupos > 10) { alert('No hay tantos cupos disponibles para este curso'); }
-      else {
-        setCupos(cupos + 1);
-      }
-    }
-    const remove = ()=>{
-      if (cupos>1){
-        setCupos (cupos-1)
-      } else {
-        alert ('No puedes agregar menos de 1 cupo al carrito!')
-      }
-    }
-
-    const addToCart = () => {
-      setFlag(false);
-  }
-    
+    const { addToCart, showHideCart } = useContext(CartContext);
+  
+  const onAddHandle = (counter) => {
+    showHideCart(false)
+    addToCart(curso, counter)
+  };
+  
+  
     return (
-            <>
-    <div className="card text-center bg-dark animate__animated animate__fadeInUp">
-      <div className="overflow">
-        <img src={curso.foto} alt="a wallpaper" className="card-img-top" />
+      <div className="container-fluid row col-11 col-sm-9 mt-5 align-content-center justify-content-center">
+        <div className="col-12 col-lg-7 align-self-center">
+          <img className="img-fluid" src={curso.foto} alt="s" />
+        </div>
+        <div className="col-12 col-lg-5 mb-3 row justify-content-center">
+          <div className="single_product_desc row justify-content-center">
+            <div className="product-meta-data mb-3">
+              <div className="line"></div>
+              <p className="product-price">${curso.price}</p>
+  
+              <h3>{curso.tittle}</h3>
+  
+              {/* <!-- Ratings & Review --> */}
+              <div className="ratings-review mb-15 d-flex align-items-center justify-content-between">
+                <div className="ratings">
+                  <i className="fa fa-star" aria-hidden="true"></i>
+                  <i className="fa fa-star" aria-hidden="true"></i>
+                  <i className="fa fa-star" aria-hidden="true"></i>
+                  <i className="fa fa-star" aria-hidden="true"></i>
+                  <i className="fa fa-star" aria-hidden="true"></i>
+                </div>
+                <div className="review">
+                  <a href="/#">Escribe una reseña</a>
+                </div>
+              </div>
+              <p className="avaibility">
+                <i className="fa fa-circle"></i> Hay stock
+              </p>
+  
+            </div>
+            {/* Cupos */}
+            <div className="col-12 dropdown mb-2 px-2 row justify-content-start">
+              <button
+                className="btn btn-secondary dropdown-toggle col-5"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Cursos
+              </button>
+              <ul
+                className="dropdown-menu "
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    <b>4</b> <span style={{ fontSize: `80%` }}>(Fotografia deportiva)</span>
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    <b>5</b> <span style={{ fontSize: `80%` }}>(Fotografia de eventos)</span>
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    <b>6</b> <span style={{ fontSize: `80%` }}>(Fotografia empresarial)</span>
+                  </a>
+                </li>
+                </ul>
+            </div>
+            {/* Descripcion */}
+            <div className="short_overview my-5">
+              <p>
+                {curso.description}
+              </p>
+            </div>
+  
+            {/* <!-- Add to Cart Form --> */}
+            {<StockContador inicial={0} stock={curso.stock} id={curso.id} onAdd={onAddHandle} texto='Agregar al carrito' /> }
+          </div>
+          
+        </div>
+        <button className="btn col-6 col-lg-3 mt-3 btnSeguirComprando"><NavLink className='fs-6' to={'/'}>Volver</NavLink></button>
       </div>
-      <div className="card-body text-light">
-        <h4 className="card-title">{curso.title}</h4>
-        <p className="card-text text-secondary">{curso.description}</p>
-        <p> {Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(curso.price)}</p>
-        {flag 
-        ? (<StockContador value={cupos} sumarProducto={add} quitarProducto ={remove} onAdd={addToCart}/>) 
-        :( <Link to="/cart"><Button variant="primary" className="addToCart">Añadir al carrito</Button></Link>)}
-         </div>
-    </div>
- </>
-)
-}
+    );
+  };
 
-export default ItemDetails;
-
-
+  export default ItemDetails;
